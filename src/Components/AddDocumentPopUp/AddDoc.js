cd95b4b5054c551c2c260978a29f3bdb6ed80a70
Tarmity <tarmity.flassman@gmail.com>
2021-07-23 14:50:25 +1000

    added the new button for the popup

---
+++ src/Components/AddDocumentPopUp/AddDoc.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import styled from 'styled-components';
+
+
+export const AddDoc = ({ showPopup, setShowPopup}) => {
+
+
+    return (
+        <>
+        {showPopup ? <h1>PopUp</h1> : null}
+            
+        </>
+    )
+}
--- src/Pages/ComplianceRegister.js
+++ src/Pages/ComplianceRegister.js
@@ -1,16 +1,44 @@
-import React from 'react'; 
+import React, { useState } from 'react';
 import styled from "styled-components";
+import { AddDoc } from '../Components/AddDocumentPopUp/AddDoc';
 
+const Container = styled.div`
+    background: lightblue;
+    margin: 1rem 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
 
+const Button = styled.button`
+   min-width: 100px;
+   padding: 12px 32px;
+   border-radius: 4px;
+   border: none;
+   background: red;
+   color: #fff;
+   font-size: 16px;
+   cursor: pointer;
+
+    
+`
 
 
 const ComplianceRegister = () => {
+    const [showPopup, setShowPopup] = useState(false)
 
- 
+    const openPopup = () => {
+        setShowPopup(prev => !prev);
+    };
+
     return (
-        <div>
+        <>
             <h1 style={{ textAlign: "center" }}>Compliance Register</h1>
-        </div>
+            <Container>
+                <Button  onClick={openPopup}>+ NEW</Button>
+                <AddDoc showPopup={showPopup} setShowPopup={setShowPopup} />
+            </Container>
+        </>
     )
 }
 
